0c62e37ca550abef6ceb25c4eebb7f683f3cb531
Mani9723 <38226555+mani9723@users.noreply.github.com>
2018-08-04 06:39:22 -0400

    Bug Fixes
    Signed-off-by: Mani9723 <38226555+Mani9723@users.noreply.github.com>

--- src/Machine/Application/Main/ApplicationMain.java
+++ src/Machine/Application/Main/ApplicationMain.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -14,7 +15,8 @@ public class ApplicationMain extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("..\\FXMLs\\loginPage.fxml"));
         primaryStage.setTitle("Bank of American");
         primaryStage.setScene(new Scene(root));
-        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.getIcons().add(new Image("Machine/images/240_F_119004171_qekVyleykNEcYt11JGhyI8scUcsLMxv2.jpg"));
+        primaryStage.initStyle(StageStyle.DECORATED);
         primaryStage.show();
     }
 
